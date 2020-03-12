f1f3d1073c2d0f11c9a0fb6070beb08dcf57164c
Evgeny Karasev <gnomskg@gmail.com>
2020-03-12 16:47:14 +0300

    style: fix code style, add repository path to config

--- packages/articles-searcher-service/app.js
+++ packages/articles-searcher-service/app.js
@@ -1,48 +1,44 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const config = require("config");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const config = require('config');
 
-const { createService } = require("@articles-searcher/api-service");
-const {
-  createModel: createCrossrefModel
-} = require("@articles-searcher/crossref-model");
-const {
-  createModel: createScopusModel
-} = require("@articles-searcher/scopus-model");
+const { createService } = require('@articles-searcher/api-service');
+const { createModel: createCrossrefModel } = require('@articles-searcher/crossref-model');
+const { createModel: createScopusModel } = require('@articles-searcher/scopus-model');
 
-const indexRouter = require("./routes/index");
+const indexRouter = require('./routes/index');
 
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 const crossrefModel = createCrossrefModel({
   rows: config.crossref.rows,
   agent: {
     name: config.crossref.agentName,
     version: config.crossref.agentVersion,
-    mailTo: config.crossref.mailTo
-  }
+    mailTo: config.crossref.mailTo,
+  },
 });
-createService(app, crossrefModel, "/crossref");
+createService(app, crossrefModel, '/crossref');
 
 const scopusModel = createScopusModel({
   rows: config.scopus.rows,
-  apiKey: config.scopus.apiKey
+  apiKey: config.scopus.apiKey,
 });
-createService(app, scopusModel, "/scopus");
+createService(app, scopusModel, '/scopus');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,11 +49,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
